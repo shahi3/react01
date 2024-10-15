a6db6e0f49680694be87486999068bf49bc39db4
shail gusain <shahilgusain777@gmail.com>
2024-10-15 08:24:53 +0530

    new file update

---
+++ o1vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Dummy from './dummy.jsx'
+
+
+function App() {
+ return(
+  <>
+    <Dummy/>
+    <h1>welcome to the web world |shahil gusain| hellow</h1>
+     
+  </>
+ )
+}
+
+export default App
---
+++ o1vitereact/src/dummy.jsx
@@ -0,0 +1,5 @@
+function Dummy (){
+    <h2>dummy i am happy with it </h2>
+
+}
+export default Dummy
